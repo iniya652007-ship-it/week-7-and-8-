3567da07fee33a6376d5a25348354517ba4a9e3c
iniya652007-ship-it <iniya652007@gmail.com>
2026-04-16 09:29:31 +0530

    Add files via upload

---
+++ WindChill.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class WindChill {
+
+    public static double calculateWindChill(double temp, double speed) {
+        return 35.74 + 0.6215 * temp +
+               (0.4275 * temp - 35.75) * Math.pow(speed, 0.16);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter temperature: ");
+        double temp = sc.nextDouble();
+
+        System.out.print("Enter wind speed: ");
+        double speed = sc.nextDouble();
+
+        System.out.println("Wind Chill: " + calculateWindChill(temp, speed));
+    }
+}
